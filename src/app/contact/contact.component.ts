3d93d380920e5a246d99a84b90e2be52812a4a38
irfan <irfanhossain2025@gmail.com>
2022-07-16 22:39:05 +0600

    fis

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -9,8 +9,8 @@ export class ContactComponent implements OnInit {
   public contactInfo = {
     title: "Contact Me ☎️",
     subtitle: "Discuss a project or just want to say hi? My Inbox is open for all.",
-    number: "+91-9634018431",
-    email_address: "sharthak31@gmail.com"
+    number: "+8801740058331",
+    email_address: "irfanhossain2025@gmail.com"
   };
   constructor() { }
 
